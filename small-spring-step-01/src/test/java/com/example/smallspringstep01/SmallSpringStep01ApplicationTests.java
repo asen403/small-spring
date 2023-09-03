3259091f0c17c3a47034616e4dd9b2d1eeb6cfa3
吴海森 <wuhaisen@moredian.com>
2023-09-03 16:19:15 +0800

    初始化项目

---
+++ small-spring-step-01/src/test/java/com/example/smallspringstep01/SmallSpringStep01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smallspringstep01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallSpringStep01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/com/edws/smallspring/SmallSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edws.smallspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
